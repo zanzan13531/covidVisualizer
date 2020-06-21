5cc3a1cc3c114faecd1ee78f7a8443fcebb54f3c
zanzan13531 <zanzan13531@gmail.com>
2020-06-21 16:43:30 -0700

    more minor styling changes

--- script.js
+++ script.js
@@ -284,7 +284,7 @@ function fillStateOverviewTable() {
             var stateAbbreviation = stateAbbreviations[stateNumber]
             var stateChartName = stateAbbreviation + "chart";
             chartHolder.style.height = "200px";
-            chartHolder.style.width = "30%";
+            chartHolder.style.width = "27%";
             chartHolder.style.margin = "auto";
             chartHolder.id = stateChartName;
             stateOverviewTableCells[stateNumber].appendChild(chartHolder);
